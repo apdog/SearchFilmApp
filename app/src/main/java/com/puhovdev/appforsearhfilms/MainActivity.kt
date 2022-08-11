e9993247585b84f2ca6fdd65535c595dd31f0a7a
Apdog <alex.puhov@gmail.com>
2022-08-11 07:43:19 +0300

    Добавлено View Binding и material bar

--- app/src/main/java/com/puhovdev/appforsearhfilms/MainActivity.kt
+++ app/src/main/java/com/puhovdev/appforsearhfilms/MainActivity.kt
@@ -1,30 +1,48 @@
 package com.puhovdev.appforsearhfilms
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.puhovdev.appforsearhfilms.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var actMainBind: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        actMainBind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(actMainBind.root)
 
-        val button1 = findViewById<Button>(R.id.central_menu_button_1)
-        button1.setOnClickListener {
+        actMainBind.centralMenuButton1.setOnClickListener{
             Toast.makeText(applicationContext, resources.getString(R.string.button_1), Toast.LENGTH_SHORT).show()
         }
-        val button2 = findViewById<Button>(R.id.central_menu_button_2)
-        button2.setOnClickListener {
+        actMainBind.centralMenuButton2.setOnClickListener {
             Toast.makeText(applicationContext, resources.getString(R.string.button_2), Toast.LENGTH_SHORT).show()
         }
-        val button3 = findViewById<Button>(R.id.central_menu_button_3)
-        button3.setOnClickListener {
+        actMainBind.centralMenuButton3.setOnClickListener {
             Toast.makeText(applicationContext, resources.getString(R.string.button_3), Toast.LENGTH_SHORT).show()
         }
-        val button4 = findViewById<Button>(R.id.central_menu_button_4)
-        button4.setOnClickListener {
+        actMainBind.centralMenuButton4.setOnClickListener {
             Toast.makeText(applicationContext, resources.getString(R.string.button_4), Toast.LENGTH_SHORT).show()
+        }
+        actMainBind.topAppBar.setNavigationOnClickListener {
+            Toast.makeText(this, "Здесь соро будет меню", Toast.LENGTH_SHORT).show()
+        }
+        actMainBind.topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.search -> {
+                    Toast.makeText(this, "Поиск", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.favourite -> {
+                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.more -> {
+                    Toast.makeText(this, "Больше", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
